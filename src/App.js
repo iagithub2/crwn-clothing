9e3f43da9723ae2570dbcc22b41150943ced4b44
Ilies <iliesassad2@gmail.com>
2021-12-17 00:24:18 +0100

    update our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import { Routes, Route, useRoutes } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
 function App() {
+  let routes = useRoutes([
+    { path: "/", element: <HomePage /> },
+    { path: "shop/hats", element: <HatsPage /> },
+    // ...
+  ]);
   return (
     <div className="App">
-      <HomePage />
+      {routes}
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { useNavigate  } from 'react-router-dom';
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => (
-    <div className={`${size} menu-item`}>
+const MenuItem = ({ title, imageUrl, size, linkUrl }) => {
+    const navigate = useNavigate();
+
+    return (<div 
+        className={`${size} menu-item`} 
+        onClick={() => navigate(linkUrl)}>
         <div 
             className='background-image'
             style={{
@@ -13,6 +18,6 @@ const MenuItem = ({ title, imageUrl, size }) => (
             <span className='subtitle'>SHOP NOW</span>
         </div>
     </div>
-);
+)};
 
 export default MenuItem;
